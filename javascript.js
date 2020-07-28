6792db89649b6d0807ebce993a03df6a4d3f9e36
Elham Fabe <elhamfabe@gmail.com>
2020-07-27 20:14:35 -0400

    new files, brainstorming

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let startQuestions = [
+    { questions: "What color is a warning sign?", answer : 4 },
+    { questions: "The most common type of parking on city streets is?", answer : 4},
+    { questions: "When driving at night in a heavy fog, you must use:", answer: 1},
+    {questions: "A broken white center line on the roadway means:", answer: 2},
+    {questions: "When passing a bicyclist, you should:", answer: 2},
+];
+ 
+let userAnswers = [
+    {}
+]
+
+let userScore = 0;
+
+for (var i = 0; i < startQuestions.length; i ++) {
+    var 
+}
+
+
+
